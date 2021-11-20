1d6caaec3163659b6b2b6e8e75dc350829b1daf8
seven-dev7 <seven.tdr7@gmail.com>
2021-11-20 19:43:54 +0100

    add 2 methods to name

--- lib/name.rb
+++ lib/name.rb
@@ -1,6 +1,8 @@
 class Name
 
-  def initialize
+  def initialize(first_name:, last_name:)
+    @first_name = first_name
+    @last_name = last_name
   end
 
   def length
@@ -9,5 +11,13 @@ class Name
 
   def human
     "I am #{length}"
+  end
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def full_name_length
+    full_name.split(' ').map{ |word| word.length }.sum
   end
 end
